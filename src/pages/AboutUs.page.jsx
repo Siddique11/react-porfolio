b1f21deaa70cc041d994062bc52645994ab9e1b7
Siddique <absiddique10@yahoo.com>
2020-12-16 00:31:46 +0600

    2. About Section

---
+++ src/pages/AboutUs.page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AboutSection from "../component/AboutSection.component";
+
+const AboutUs = () => (
+  <div>
+    <AboutSection />
+  </div>
+);
+
+export default AboutUs;
