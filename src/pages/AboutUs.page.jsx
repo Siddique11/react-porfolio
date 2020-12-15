e529a5a342e01a9d541f4290e63d4fa75af106e1
Siddique <absiddique10@yahoo.com>
2020-12-16 01:02:42 +0600

    4. Faq Section

--- src/pages/AboutUs.page.jsx
+++ src/pages/AboutUs.page.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import AboutSection from "../component/AboutSection.component";
+import FaqSection from "../component/FaqSection.component";
+import ServicesSection from "../component/ServicesSection.component";
 
 const AboutUs = () => (
-  <div>
+  <>
     <AboutSection />
-  </div>
+    <ServicesSection />
+    <FaqSection />
+  </>
 );
 
 export default AboutUs;
