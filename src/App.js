1912c7a9b5ecd6cff6a52d929effe0e77c119e6c
Siddique <absiddique10@yahoo.com>
2020-12-16 17:54:34 +0600

    5. Intro to Style component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import GlobalStyle from "./component/GlobalStyle.style";
 import AboutUs from "./pages/AboutUs.page";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
